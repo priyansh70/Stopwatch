6bb0dc6eda1f5afedbf4b2cb9ba2a03eecad8cae
Priyansh Patel <93965871+priyansh70@users.noreply.github.com>
2022-08-31 00:08:19 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,78 +1,84 @@
-let startBtn = document.getElementById('start');
-let stopBtn = document.getElementById('stop');
-let resetBtn = document.getElementById('reset');
+let hour = document.getElementById('hour');
+let minute = document.getElementById('minute');
+let second = document.getElementById('second');
+let msecond = document.getElementById('msecond');
+let start = document.getElementById('start');
+let stop = document.getElementById('stop');
+let reset = document.getElementById('reset');
 
-let hour = 00;
-let minute = 00;
-let second = 00;
-let count = 00;
+let hr = 0;
+let min = 0;
+let sec = 0;
+let count = 0;
 
-startBtn.addEventListener('click', function () {
+let timer = false;
+
+start.addEventListener('click', function () {
     timer = true;
-    stopWatch();
+    stopwatch();
 });
 
-stopBtn.addEventListener('click', function () {
+stop.addEventListener('click', function () {
     timer = false;
 });
 
-resetBtn.addEventListener('click', function () {
+
+reset.addEventListener('click', function () {
     timer = false;
-    document.getElementById('hr').innerHTML = "00";
-    document.getElementById('min').innerHTML = "00";
-    document.getElementById('sec').innerHTML = "00";
-    document.getElementById('count').innerHTML = "00";
+    hr = 0;
+    min = 0;
+    sec = 0;
+    count = 0;
+    hour.textContent = "00";
+    minute.textContent = "00";
+    second.textContent = "00";
+    msecond.textContent = "00";
 });
 
-function stopWatch() {
+function stopwatch() {
     if (timer) {
         count++;
-
         if (count == 100) {
-            second++;
+            sec++;
             count = 0;
         }
 
-        if (second == 60) {
-            minute++;
-            second = 0;
+        if (sec == 60) {
+            min++;
+            sec = 0;
         }
 
-        if (minute == 60) {
-            hour++;
-            minute = 0;
-            second = 0;
+        if (min == 60) {
+            hr++;
+            min = 0;
+            sec = 0;
         }
 
-        let hrString = hour;
-        let minString = minute;
-        let secString = second;
+        let hrString = hr;
+        let minString = min;
+        let secString = sec;
         let countString = count;
 
-        if(hour < 10)
-        {
+        if (hr < 10) {
             hrString = "0" + hrString;
         }
 
-        if(minute < 10)
-        {
+        if (min < 10) {
             minString = "0" + minString;
         }
 
-        if(second < 10)
-        {
+        if (sec < 10) {
             secString = "0" + secString;
         }
 
-        if(count < 10)
-        {
+        if (count < 10) {
             countString = "0" + countString;
         }
 
-        document.getElementById('hr').innerHTML = hrString;
-        document.getElementById('min').innerHTML = minString;
-        document.getElementById('sec').innerHTML = secString;
-        document.getElementById('count').innerHTML = countString;
-        setTimeout(stopWatch, 10);
+        hour.textContent = hrString;
+        minute.textContent = minString;
+        second.textContent = secString;
+        msecond.textContent = countString;
+        setTimeout(stopwatch, 10);
     }
 }
